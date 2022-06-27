3a8258f12d0d8dd7b41b3a21cbc4118f0e6808b7
E Kline <EZchaird@gmail.com>
2022-06-27 15:48:23 -0500

    Multigauss V2 - added functionality to measure.py as well
    Round 2 of changes and fixes to the multigauss routine. Squashed interesting bugs and added a few checks, in event of high error or running out of iterations.
    For measure.py, the only change was allowing an overlay, if desired, using multigauss's gaussian filter. This can aid in visualization of noisier data. 
    As a result, two new options have been added to reduce.py's parser: '-overlay', which causes the gaussian convolution to be shown when running measure.py, and '-multigauss', which tells it to additionally (or exclusively!) run the many gaussian fit.

--- pyappss/scripts/reduce.py
+++ pyappss/scripts/reduce.py
@@ -2,13 +2,24 @@ import argparse
 
 from analysis import baseline
 from analysis import measure
+from analysis import multigauss as multigaussfit
 
-
-def main(agc, smo=None, gauss=False, twopeak=False, trap=False, light_mode=False, noconfirm=False):
+def main(agc, smo=None, gauss=False, twopeak=False, trap=False, multigauss=False, light_mode=False, noconfirm=False, overlay=False):
+    if multigauss == True:
+        if smo is None:
+            smo = 21
+        elif smo > 21:
+            smo = 21
+            #Multigauss is untested with no smoothing, and probably will not work with a light boxcar or no boxcar. 21 is the tried and true
+            #value.
+    
     b = baseline.Baseline(agc, smooth_int=smo, noconfirm=noconfirm)
     vel, spec, rms = b()
-    measure.Measure(smo=smo, gauss=gauss, twopeak=twopeak, trap=trap, light_mode=light_mode,
-                    vel=vel, spec=spec, rms=rms, agc=agc, noconfirm=noconfirm)
+    if gauss or twopeak or trap:
+        measure.Measure(smo=smo, gauss=gauss, twopeak=twopeak, trap=trap, light_mode=light_mode,
+                    vel=vel, spec=spec, rms=rms, agc=agc, noconfirm=noconfirm, overlay=overlay)
+    if multigauss == True:
+        multigaussfit.ManyGauss(vel=vel, spec=spec, rms=rms, agc=agc)
 
 
 if __name__ == '__main__':
@@ -22,9 +33,13 @@ if __name__ == '__main__':
     parser.add_argument('-gauss', action='store_true', help='Do a Gaussian fit of the spectrum')
     parser.add_argument('-twopeak', action='store_true', help='Do a Two Peak fit of the spectrum')
     parser.add_argument('-trap', action='store_true', help='Do a Trapezoidal fit of the spectrum')
+    #New routine!
+    parser.add_argument('-multigauss', action='store_true', help='Do a Many Gaussian fit of the spectrum, calculated near-automatically. As a note, this does take noticeably longer than other methods, typically')
     parser.add_argument('-pub', action='store_true', help='Plot in Light Mode which is good for publication')
     parser.add_argument('-noconfirm', action='store_true', help='Additional option to remove confirmations, both for baseline selection and for fit model choice')
+    parser.add_argument('-overlay', action='store_true', help='Additional option to display a helpful filter overlay when plotting during main measure routine')
 
+    
     args = parser.parse_args()
-    main(agc=args.filename, smo=args.smo, gauss=args.gauss, twopeak=args.twopeak, trap=args.trap,
-         light_mode=args.pub, noconfirm=args.noconfirm)
+    main(agc=args.filename, smo=args.smo, gauss=args.gauss, twopeak=args.twopeak, trap=args.trap, multigauss=args.multigauss,
+         light_mode=args.pub, noconfirm=args.noconfirm, overlay=args.overlay)
