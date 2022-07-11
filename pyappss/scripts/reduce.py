e6a7d17de4cdf50ead02087b9af15ce0c777150d
Haille <hmlperkins@hotmail.com>
2022-07-10 20:52:27 -0400

    Incorperate Multigauss into betatest
    Functions exactly the same as the multigauss in feature-manygauss, but the user cannot run both multigauss and any of the other fits to avoid the forced smoothing for now

--- pyappss/scripts/reduce.py
+++ pyappss/scripts/reduce.py
@@ -1,13 +1,13 @@
 import argparse
 
-from pyappss.analysis import baseline
-from pyappss.analysis import measure
+from analysis import baseline
+from analysis import measure
+from analysis import multigauss
 
 
 def reduce():
     # NOTE: you need to have the FITS file in the same directory as this script!
-    parser = argparse.ArgumentParser(
-        description="Baseline and Reduce HI Spectrum.")
+    parser = argparse.ArgumentParser(description="Baseline and Reduce HI Spectrum.")
     parser.add_argument('filename', metavar='AGC', nargs='+', type=str, help="AGC number of the galaxy, e.g, 104365. "
                                                                              "You may list multiple. e.g. 3232 104365 1993 ...")
     parser.add_argument('-smo', metavar='smooth', type=int,
@@ -21,6 +21,10 @@ def reduce():
     parser.add_argument('-noconfirm', action='store_true',
                         help='Additional option to remove confirmations, both for baseline selection and for fit model choice')
 
+    # new routine!
+    parser.add_argument('-multigauss', action='store_true',
+                        help='Do a Many Gaussian fit of the spectrum, calculated near-automatically. As a note, this does take noticeably longer than other methods, typically.')
+    parser.add_argument('-overlay', action='store_true', help='Additional option to display a helpful filter overlay when plotting during main measure routine')
     args = parser.parse_args()
 
     agcs = args.filename
@@ -30,15 +34,31 @@ def reduce():
     trap = args.trap
     dark_mode = args.dark_mode
     noconfirm = args.noconfirm
+    mgauss = args.multigauss
+    overlay = args.overlay
+
 
     for agc in agcs:
 
         try:
 
-            b = baseline.Baseline(agc, smooth_int=smo, noconfirm=noconfirm, dark_mode=dark_mode)
-            vel, spec, rms = b()
-            measure.Measure(smo=smo, gauss=gauss, twopeak=twopeak, trap=trap, dark_mode=dark_mode,
-                            vel=vel, spec=spec, rms=rms, agc=agc, noconfirm=noconfirm)
+            if not mgauss:
+                b = baseline.Baseline(agc, smooth_int=smo, noconfirm=noconfirm, dark_mode=dark_mode)
+                vel, spec, rms = b()
+                measure.Measure(smo=smo, gauss=gauss, twopeak=twopeak, trap=trap, dark_mode=dark_mode,
+                                vel=vel, spec=spec, rms=rms, agc=agc, noconfirm=noconfirm, overlay=overlay)
+
+            if mgauss:
+                if smo is None:
+                    smo = 21
+                elif smo > 21:
+                    smo = 21
+
+                # odd fix for now - only weird for people who do a fit type above and then try multigauss
+                b = baseline.Baseline(agc, smooth_int=smo, noconfirm=noconfirm, dark_mode=dark_mode)
+                vel, spec, rms = b()
+                multigauss.ManyGauss(vel=vel, spec=spec, rms=rms, agc=agc)
+
         except IOError:
             print("Could not open AGC{}.fits\n".format(agc))
 
